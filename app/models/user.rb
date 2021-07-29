193c008a7b929fd46563d667a4d085ebf6eb8016
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-07-29 14:42:32 -0400

    logout added to application layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :meals
     has_many :restaurants, through: :meals
     has_many :reviews, through: :meals
+    validates :email, :password, presence: true
+    validates :email, uniqueness: true
     has_secure_password
 end
