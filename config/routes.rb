81ee8a387abbe75961b6a0df757101151af5fd36
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-07-30 13:55:30 -0400

    flash messages/error messages in views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   resources :restaurants do 
     resources :meals, shallow: true
   end
-  resources :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
