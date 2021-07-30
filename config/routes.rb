72e4c26c141eea698ee0655da4306a323a7d3ae6
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-07-30 14:24:40 -0400

    added nested route show page to meals

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   resources :reviews
   resources :locations
-  resources :meals, only: [:new, :index, :create]
+  resources :meals, only: [:new, :index, :create, :show]
   resources :restaurants do 
-    resources :meals, shallow: true
+    resources :meals, only: [:new, :index, :create, :show]
   end
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_210335) do
+ActiveRecord::Schema.define(version: 2021_07_30_181343) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_07_24_210335) do
     t.integer "restaurant_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "restaurants", force: :cascade do |t|
